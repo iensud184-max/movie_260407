b77ece867ca7f538c2b5a7c2b6b357220298bc22
iensud184-max <iensud184@gmail.com>
2026-04-09 17:50:37 +0900

    260409_KDH

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    # app.config.from_object('config')
 
-    @app.route('/')
-    def index():
-        return 'movie'
-
+    #블루프린트 등록
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
+    
     return app
